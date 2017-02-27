ad3c0e86764da02bf4dbdc4293ae144c9c95044b
Bram de Greve <bram.degreve@bramz.net>
2017-02-27 10:16:56 +0100

    Attempt at better UX by vibrating on important changes

--- app/components/geocache.js
+++ app/components/geocache.js
@@ -4,37 +4,71 @@ import { connect } from 'react-redux'
 import Loading from 'components/loading'
 import Number from 'components/number'
 import Ripple from 'components/ripple'
+import vibrate from 'lib/vibrate'
 
 import styles from './geocache.css'
 
-const View = ({waypoint, distance, accuracy, isFetching, isFinished, isFurther}) => {
-  if (isFinished) {
-    return <img src={require('img/finish.svg')} className={styles.finished} />
+class View extends React.Component {
+  componentDidMount () {
+    vibrate(300)
   }
-  if (isFetching) {
-    return <Loading />
+
+  componentDidUpdate (prevProps) {
+    const { isFinished, isFurther, waypoint } = this.props
+
+    if (isFinished && !prevProps.isFinished) {
+      vibrate([300, 200, 300, 200, 300])
+      return
+    }
+    if (isFurther && !prevProps.isFurther) {
+      vibrate([300, 200, 300])
+      return
+    }
+    if (waypoint !== prevProps.waypoint) {
+      vibrate(300)
+    }
   }
-  const {dist, unit} = distance > 2000
-    ? {dist: distance / 1000, unit: 'km'}
-    : {dist: distance, unit: 'm'}
-  return (
-    <div className={styles.container}>
-      <div className={styles.waypoint}>
-        {waypoint + 1}
-      </div>
-      <div className={styles.grow}>
-        <div className={styles.child}>
-          <Ripple
-            dur={duration(distance)}
-            stroke={isFurther ? '#fa5252' : '#51cf66'}
-          />
+
+  render () {
+    const {
+      waypoint,
+      distance,
+      isFetching,
+      isFinished,
+      isFurther
+    } = this.props
+
+    if (isFinished) {
+      return <img src={require('img/finish.svg')} className={styles.finished} />
+    }
+
+    if (isFetching) {
+      return <Loading />
+    }
+
+    const {dist, unit} = distance > 2000
+      ? {dist: distance / 1000, unit: 'km'}
+      : {dist: distance, unit: 'm'}
+
+    return (
+      <div className={styles.container}>
+        <div className={styles.waypoint}>
+          {waypoint + 1}
+        </div>
+        <div className={styles.grow}>
+          <div className={styles.child}>
+            <Ripple
+              dur={duration(distance)}
+              stroke={isFurther ? '#fa5252' : '#51cf66'}
+            />
+          </div>
+        </div>
+        <div className={styles.distance}>
+          <Number value={dist} fixed />{unit}
         </div>
       </div>
-      <div className={styles.distance}>
-        <Number value={dist} fixed />{unit}
-      </div>
-    </div>
-  )
+    )
+  }
 }
 
 const mapStateToProps = ({geocache, location}) => ({
---
+++ app/lib/vibrate.js
@@ -0,0 +1,14 @@
+export default (pattern) => {
+  if (typeof navigator === 'undefined') {
+    return false
+  }
+  const _vibrate = navigator.vibrate || navigator.webkitVibrate || navigator.mozVibrate || navigator.msVibrate
+  if (!_vibrate) {
+    return false
+  }
+  try {
+    return _vibrate(pattern)
+  } catch (err) {
+    return false
+  }
+}
